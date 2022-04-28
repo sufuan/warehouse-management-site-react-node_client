282febddd8901cdd24a99334056d29e92c3a8851
sufuan <abu49539@gmail.com>
2022-04-29 00:28:29 +0600

    dummy data loaded

--- src/component/Inventory/Inventory.js
+++ src/component/Inventory/Inventory.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import InventoryDetails from '../InventoryDEtails/InventoryDetails';
 
 const Inventory = () => {
+
+    const [inventories, setInventories] = useState([])
+
+    useEffect(() => {
+
+        fetch('books.json')
+            .then(res => res.json())
+            .then(data => setInventories(data))
+
+    }, [])
     return (
         <div>
-                inventory
+                 <div className='w-90vh px-4 pt-10 pb-24 mx-auto max-w-7xl md:px-2'>
+            <div className="text-center">
+                <h1 className='text-4xl py-10 ' > our product</h1>
+            </div>
+
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 justify-items-center">
+                {
+                    inventories.map(inventory => <InventoryDetails
+                        key={inventory.id}
+                        inventory={inventory}
+                    ></InventoryDetails>)
+                }
+            </div>
+        </div>
         </div>
     );
 };
