965f0e5c2d72ed3fedcc35d2a150ce57df2ef8bc
sufuan <abu49539@gmail.com>
2022-05-02 00:36:08 +0600

    update

--- src/component/InventoryDEtails/InventoryDetails.js
+++ src/component/InventoryDEtails/InventoryDetails.js
@@ -7,27 +7,57 @@ const InventoryDetails = () => {
     const [product, setProduct] = useState({})
     const { description, img, name, supplier, quantity } = product
 
+
+    const [updateQuantity, setUpdateQuantity] = useState(quantity)
+    useEffect(() => {
+        setUpdateQuantity(quantity)
+    }, [quantity])
+
+    console.log(updateQuantity);
+
     useEffect(() => {
         fetch(`http://localhost:4000/product/${id}`)
             .then(res => res.json())
             .then(data => {
-                // console.log(data)
+                console.log(data)
                 setProduct(data)
             })
-    }, [])
+    }, [id])
 
 
-    const updateName=(e)=>{
+    const handleAddItem = (e) => {
         e.preventDefault()
-        const quantity = e.target.quantity.value
-        console.log(quantity);
+        const quantity = +(e.target.quantity.value)
+        // console.log(newQuan);
+
+        console.log(typeof quantity);
 
         fetch(`http://localhost:4000/product/${id}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({quantity:quantity}),
+            body: JSON.stringify({ quantity: quantity }),
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+
+
+    }
+
+
+
+    const handleDecrease = () => {
+
+        console.log(updateQuantity);
+        setUpdateQuantity(updateQuantity - 1)
+
+        fetch(`http://localhost:4000/product/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ updateQuantity: updateQuantity }),
         })
             .then((res) => res.json())
             .then((data) => console.log(data));
@@ -37,13 +67,19 @@ const InventoryDetails = () => {
 
 
     return (
+
         <div className="bg-white">
             <div className="pt-6 pb-16 sm:pb-24">
 
                 <div className="mt-8 max-w-2xl mx-auto px-4 sm:px-6 lg:max-w-7xl lg:px-8">
                     <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
                         <div className="lg:col-start-8 lg:col-span-5">
+                            <div className="flex items-center justify-between">
                             <h1 className="text-xl font-medium text-gray-900">{name}</h1>
+                            <h2 className="text-xl font-medium text-gray-900">Quantity:{updateQuantity}</h2>
+                            </div>
+                           
+
                             <div className="mt-4">
 
                                 <div className="flex items-center">
@@ -66,31 +102,39 @@ const InventoryDetails = () => {
                             </div>
                         </div>
 
+
+
                         <div className="mt-8 lg:col-span-5">
-                            <form onSubmit={updateName}>
+                            <button
+                                onClick={handleDecrease}
+                                className="mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                            >
+                                deliver
+                            </button>
+                            <form onSubmit={handleAddItem}>
 
                                 <div>
-                                    <h2 className="text-sm font-medium text-gray-900">Quantity:{quantity}</h2>
-                                    <input name='quantity' type="text" placeholder='add ' />
+                                    <input  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm my-5" name='quantity' type="number" placeholder='restock item ' required />
                                 </div>
 
                                 <button
                                     type="submit"
                                     className="mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
-                                    add
+                                    Restock
                                 </button>
 
-                              
-                            
+
+
 
                             </form>
 
 
-                           
+
 
                             {/* Product details */}
                             <div className="mt-10">
+                                <h1 className='texl-xl'>Product Description:</h1>
                                 <h2 className="text-sm font-medium text-gray-900">{description}</h2>
 
 
