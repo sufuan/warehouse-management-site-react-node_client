76175e84101594b867c7cbe233dda10afbd833c1
sufuan <abu49539@gmail.com>
2022-05-01 11:21:14 +0600

    err loading added

--- src/Auth/RequireAuth.js
+++ src/Auth/RequireAuth.js
@@ -3,6 +3,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
 // import Loading from '../component/Loading/Loading';
 import { auth } from '../firebase';
+import Loading from './Loading/Loading';
 
 const RequireAuth = ({children}) => {
     // const [user,loading] =useAuthState(auth)
@@ -10,9 +11,9 @@ const RequireAuth = ({children}) => {
     const location= useLocation()
 
 
-    // if(loading){
-    //     return <Loading/>
-    // }
+    if(loading){
+        return <Loading/>
+    }
 
     if(!user){
         return <Navigate to="/login" state={{ from: location }} replace />;
--- src/component/MyItems/MyItems.js
+++ src/component/MyItems/MyItems.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase';
 
 const MyItems = () => {
+
+    const [myitems, setMyitems] = useState([])
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+
+        const myItem = async () => {
+            // console.log(user?.email);
+
+             const email =user?.email
+             console.log(email);
+            const { data } = await axios.get(`http://localhost:4000/products?email${email}`)
+            setMyitems(data)
+        }
+        myItem()
+
+    }, [user])
+     console.log(user?.email);
     return (
         <div>
-                h
+            this is my items {myitems.length}
         </div>
     );
 };
