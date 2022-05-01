7ed1f0c0d3d143b8ef1313bf313f7088816a2fe9
sufuan <abu49539@gmail.com>
2022-05-01 21:51:41 +0600

    update

--- src/component/InventoryDEtails/InventoryDetails.js
+++ src/component/InventoryDEtails/InventoryDetails.js
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 
 const InventoryDetails = () => {
     const { id } = useParams()
-    console.log(id);
+    // console.log(id);
     const [product, setProduct] = useState({})
     const { description, img, name, supplier, quantity } = product
 
@@ -11,10 +11,30 @@ const InventoryDetails = () => {
         fetch(`http://localhost:4000/product/${id}`)
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 setProduct(data)
             })
     }, [])
+
+
+    const updateName=(e)=>{
+        e.preventDefault()
+        const quantity = e.target.quantity.value
+        console.log(quantity);
+
+        fetch(`http://localhost:4000/product/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({quantity:quantity}),
+        })
+            .then((res) => res.json())
+            .then((data) => console.log(data));
+
+
+    }
+
 
     return (
         <div className="bg-white">
@@ -47,20 +67,27 @@ const InventoryDetails = () => {
                         </div>
 
                         <div className="mt-8 lg:col-span-5">
-                            <form>
+                            <form onSubmit={updateName}>
 
                                 <div>
                                     <h2 className="text-sm font-medium text-gray-900">Quantity:{quantity}</h2>
-
+                                    <input name='quantity' type="text" placeholder='add ' />
                                 </div>
 
                                 <button
                                     type="submit"
                                     className="mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                 >
-                                    Deliver
+                                    add
                                 </button>
+
+                              
+                            
+
                             </form>
+
+
+                           
 
                             {/* Product details */}
                             <div className="mt-10">
