4c28cc37c0e294236caadd2f8c6ec90a6261318c
sufuan <abu49539@gmail.com>
2022-05-03 23:15:53 +0600

    final

--- src/component/Home page/Home.js
+++ src/component/Home page/Home.js
@@ -75,14 +75,16 @@ const Home = () => {
                             }
 
                         </div>
+                        <div className='text-center my-9'>
+                            <Link class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" to='/inventories' > Manage Inventories</Link>
+                        </div>
                     </div>
+
                 </div>
 
 
 
-                <div className='text-center my-9'>
-                    <Link class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" to='/inventories' > Manage Inventory</Link>
-                </div>
+
             </section>
 
 
